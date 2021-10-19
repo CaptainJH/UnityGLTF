092f6ae06bf012512c96f345d735f966993a875a
heqi <heqi.ju@unity3d.com>
2021-10-19 15:18:59 +0800

    add test scenes

---
+++ UnityGLTF/Assets/CameraFitter.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFitter : MonoBehaviour
+{
+	public RectTransform FitTarget = null;
+	public Camera MainCamera = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		Vector3[] worldCorners = new Vector3[4];
+		FitTarget.GetWorldCorners(worldCorners);
+		Vector2 rightTop = RectTransformUtility.WorldToScreenPoint(MainCamera, worldCorners[2]);
+		Vector2 leftBottom = RectTransformUtility.WorldToScreenPoint(MainCamera, worldCorners[0]);
+		Rect rect = new Rect(leftBottom.x / Screen.width, leftBottom.y / Screen.height,
+			(rightTop.x - leftBottom.x) / Screen.width, (rightTop.y - leftBottom.y) / Screen.height);
+
+		var thisCamera = GetComponent<Camera>();
+		thisCamera.rect = rect;
+	}
+
+
+}
